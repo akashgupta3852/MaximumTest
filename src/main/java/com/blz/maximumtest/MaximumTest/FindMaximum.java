0013de9dea40e954fd41ea9d942d97029369437b
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-08 23:13:49 +0530

    [Akash]Refactored: all the 3 getMaximum() methods to one generic method

--- src/main/java/com/blz/maximumtest/MaximumTest/FindMaximum.java
+++ src/main/java/com/blz/maximumtest/MaximumTest/FindMaximum.java
@@ -2,26 +2,8 @@ package com.blz.maximumtest.MaximumTest;
 
 public class FindMaximum {
 
-	public Integer getMaximum(Integer a, Integer b, Integer c) {
-		Integer max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public Double getMaximum(Double a, Double b, Double c) {
-		Double max = a;
-		if (b.compareTo(max) > 0)
-			max = b;
-		if (c.compareTo(max) > 0)
-			max = c;
-		return max;
-	}
-
-	public String getMaximum(String a, String b, String c) {
-		String max = a;
+	public <T extends Comparable<T>> T getMaximum(T a, T b, T c) {
+		T max = a;
 		if (b.compareTo(max) > 0)
 			max = b;
 		if (c.compareTo(max) > 0)
